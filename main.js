1ad6d10fd0840ad7c8bda4f8f7792a411396c72d
Andrés Wagner <13288723+andreswagner@users.noreply.github.com>
2023-12-01 20:50:10 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 function main(params) {
+const axios = require('axios').default;
+      axios({
+					method: "POST", // Required, HTTP method, a string, e.g. POST, GET
+					url: "http://52.117.223.57:1880/webhook",
+					data: {
+						hola: "mundo"
+						},
+					},
+					headers: {
+						"Content-Type": "application/json"
+					},
+				});
+    
       return {
           headers: { 'Content-Type': 'text/plain; charset=utf-8' },
           body: {"hola":"mundo"}
